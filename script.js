97dbb84eb755f1e60beefdcc7d0da9b1ad019cae
Muijsenberg,Reno R.F. <r.muijsenberg@student.fontys.nl>
2024-06-14 14:30:14 +0200

    Try change test and workflow a bit

--- script.js
+++ script.js
@@ -5,9 +5,13 @@ const targetUrl = __ENV.TARGET_URL;
 
 export let options = {
     stages: [
-        { duration: '30s', target: 10000 }, // ramp up to 10 users over 10 seconds
-        { duration: '1m', target: 10000 }, // stay at 10 users for 1 minute
-        { duration: '10s', target: 0 }, // ramp down to 0 users over 10 seconds
+        { duration: '30s', target: 2500 },
+        { duration: '30s', target: 2500 },
+        { duration: '30s', target: 5000 },
+        { duration: '30s', target: 5000 },
+        { duration: '30s', target: 10000 },
+        { duration: '30s', target: 10000 },
+        { duration: '30s', target: 0 },
     ],
 };
 
@@ -15,7 +19,6 @@ export default function () {
     let res = http.get(`http://${targetUrl}/travel`);
     check(res, {
         'is status 200': (r) => r.status === 200,
-        'response time < 500ms': (r) => r.timings.duration < 500,
     });
-    sleep(1); // wait for 1 second before the next iteration
+    sleep(1);
 }
